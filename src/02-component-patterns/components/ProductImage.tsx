ad5121c0bf1bb085568bec5f3bab77255dd389dc
Natalia Meneses <nmeneses8@gmail.com>
2022-06-21 13:02:37 -0500

    Component compound listo

---
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { ProductContext } from "./ProductCard";
+
+import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
+
+export const ProductImage = ({ img = "" }) => {
+  const { product } = useContext(ProductContext);
+  return (
+    <img
+      className={styles.productImg}
+      src={img ? img : product.img ?? noImage}
+      alt="Coffee Mug"
+    />
+  );
+};
